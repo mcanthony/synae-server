d4ae016a4e3832b19f1cdab9eee383fe205dfa59
theoperatore <theoperatore@gmail.com>
2015-09-14 23:41:38 -0400

    group chooser style

--- frontend/components/group-chooser.jsx
+++ frontend/components/group-chooser.jsx
@@ -8,22 +8,53 @@ export default class extends React.Component {
   }
 
   render() {
-    let groups = this.props.groups.map(g => {
-      return (
-        <button
-          key={g.id}
-          type='button'
-          className='button button-big'
-          style={{ backgroundColor: g.color }}
-          onClick={this.props.onGroupSelect.bind(this, g.id)}
-          data-groupid={g.id}
-          >{g.name}</button>
-      )
-    });
     return (
-      <div className=''>
-        <h1 className='px2'>What Group Are You?</h1>
-        <div className='flex flex-justify clearfix'>{groups}</div>
+      <div className='flex flex-column'>
+        <h1 style={{maxHeight:'10vh'}}className='px2'>Where Are You?</h1>
+        <div className='flex'>
+          <div style={{height: '35vh'}} className='flex-auto px2 py2'>
+            <button
+              key={this.props.groups[1].id}
+              type='button'
+              className='button button-big group-chooser-btn'
+              style={{ color: this.props.groups[1].color, border: '1px solid #ddd', backgroundColor: 'transparent' }}
+              onClick={this.props.onGroupSelect.bind(this, this.props.groups[1].id)}
+              data-groupid={this.props.groups[1].id}
+            >{this.props.groups[1].name}</button>
+          </div>
+          <div style={{height: '35vh'}} className='flex-auto px2 py2'>
+            <button
+              key={this.props.groups[0].id}
+              type='button'
+              className='button button-big group-chooser-btn'
+              style={{ color: this.props.groups[0].color, border: '1px solid #ddd', backgroundColor: 'transparent' }}
+              onClick={this.props.onGroupSelect.bind(this, this.props.groups[0].id)}
+              data-groupid={this.props.groups[0].id}
+            >{this.props.groups[0].name}</button>
+          </div>
+        </div>
+        <div className='flex'>
+          <div style={{height: '35vh'}} className='flex-auto px2 py2'>
+            <button
+              key={this.props.groups[2].id}
+              type='button'
+              className='button button-big group-chooser-btn'
+              style={{ color: this.props.groups[2].color, border: '1px solid #ddd', backgroundColor: 'transparent' }}
+              onClick={this.props.onGroupSelect.bind(this, this.props.groups[2].id)}
+              data-groupid={this.props.groups[2].id}
+            >{this.props.groups[2].name}</button>
+          </div>
+          <div style={{height: '35vh'}} className='flex-auto px2 py2'>
+            <button
+              key={this.props.groups[3].id}
+              type='button'
+              className='button button-big group-chooser-btn'
+              style={{ color: this.props.groups[3].color, border: '1px solid #ddd', backgroundColor: 'transparent' }}
+              onClick={this.props.onGroupSelect.bind(this, this.props.groups[3].id)}
+              data-groupid={this.props.groups[3].id}
+            >{this.props.groups[3].name}</button>
+          </div>
+        </div>
       </div>
     )
   }
